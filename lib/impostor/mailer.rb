69f1d71be3246167fa4bd0a6b5c7990e1e9e90d2
Pablo A. Torres Navarrete <tn.pablo@gmail.com>
2012-01-13 10:04:30 -0500

    Added Mailer.send_info_to_interrogator
    Right after the game starts, the system sends to each player all the
    information about the other players. This methods sends the names, email
    and descriptions of the impostor and the honest player to the
    interrogator, the user that started the game.
    
    Now there is one Mailer object per game, to handle all the email for it.
    This implies that the constructor now optionally takes a game object.

--- lib/impostor/mailer.rb
+++ lib/impostor/mailer.rb
@@ -3,8 +3,11 @@ require 'yaml'
 
 module Impostor
   class Mailer
-    def initialize(config_file="~/.impostor/config.yaml")
+    def initialize(game=nil, config_file="~/.impostor/config.yaml")
+      @game = game
       @config = parse_config(config_file)
+      @email_address = @config[:smtp][:user_name] + "@" + @config[:smtp][:domain]
+
       configure_smtp @config[:smtp]
       configure_pop3 @config[:pop3]
     end
@@ -16,6 +19,42 @@ module Impostor
     #
     def get_unread(count)
       Mail.last :count => count
+    end
+
+    def send_info_to_interrogator
+      a, b          = @game.randomized_players
+      interrogator  = @game.interrogator.email
+      me            = @email_address
+      id            = @game.id
+
+      Mail.deliver do
+        from    me
+        to      interrogator
+        subject "New game starting!"
+        body    <<-BODY
+                Hi, the new game you requested has started!
+
+                To start asking, send me mail with subject "QUESTION #{id}"
+
+                You will be playing with the following two players:
+
+                #{a.email}
+                #{a.username}
+
+                #{a.description}
+
+                #{b.email}
+                #{b.username}
+
+                #{b.description}
+        BODY
+      end
+    end
+
+    def send_info_to_impersonator
+    end
+
+    def send_info_to_honest
     end
 
     def configure_smtp(config)
