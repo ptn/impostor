66bb4e1605586e5f24f3050718f7e8354e125a97
Pablo A. Torres Navarrete <tn.pablo@gmail.com>
2012-01-13 16:59:26 -0500

    Format

--- lib/impostor/templates.rb
+++ lib/impostor/templates.rb
@@ -1,19 +1,19 @@
 module Impostor
   module Templates
     INFO_TO_INTERROGATOR = <<-BODY
-                Hi, the new game you requested has started!
+      Hi, the new game you requested has started!
 
-                To start asking, send me mail with subject "QUESTION %{game_id}"
+      To start asking, send me mail with subject "QUESTION %{game_id}"
 
-                You will be playing with the following two players:
+      You will be playing with the following two players:
 
-                %{username_a}
-                %{email_a}
+      %{username_a}
+      %{email_a}
 
-                %{description_a}
+      %{description_a}
 
-                %{username_b}
-                %{email_b}
+      %{username_b}
+      %{email_b}
 
       %{description_b}
 
