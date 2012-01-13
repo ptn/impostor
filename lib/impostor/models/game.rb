f52e8a08451f885d7ca923bddd153d1d9c1875a1
Pablo A. Torres Navarrete <tn.pablo@gmail.com>
2012-01-13 08:47:41 -0500

    Adding access methods to a game's players.

--- lib/impostor/models/game.rb
+++ lib/impostor/models/game.rb
@@ -7,6 +7,22 @@ module Impostor
 
       has n, :players
       has n, :users, :through => :players
+
+      def interrogator
+        players.first(:role => "interrogator").user
+      end
+
+      def impersonator
+        players.first(:role => "impersonator").user
+      end
+
+      def honest
+        players.first(:role => "honest").user
+      end
+
+      def random_player
+        players.all(:role.not => "interrogator").sample.user
+      end
     end
   end
 end
