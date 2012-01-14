bdfc6b3d14671c97d67b6d2a565dc403b11f3e6c
Pablo A. Torres Navarrete <tn.pablo@gmail.com>
2012-01-14 09:49:32 -0500

    Moving database setup to it's own file

--- lib/impostor.rb
+++ lib/impostor.rb
@@ -1,17 +1,5 @@
-require 'data_mapper'
+require 'impostor/setup'
 require 'impostor/server'
 
 module Impostor
-  base = File.expand_path("~/.impostor")
-  DataMapper::Logger.new(File.join(base, 'db.log'), :debug)
-  DataMapper.setup(:default, 'sqlite://' + File.join(base,'impostor.db'))
 end
-
-require 'impostor/user'
-require 'impostor/game'
-require 'impostor/question'
-require 'impostor/answer'
-require 'impostor/player'
-
-DataMapper.finalize
-DataMapper.auto_upgrade!
---
+++ lib/impostor/setup.rb
@@ -0,0 +1,17 @@
+require 'data_mapper'
+
+module Impostor
+  base = File.expand_path("~/.impostor")
+  DataMapper::Logger.new(File.join(base, 'db.log'), :debug)
+  DataMapper.setup(:default, 'sqlite://' + File.join(base,'impostor.db'))
+end
+
+
+require 'impostor/user'
+require 'impostor/game'
+require 'impostor/question'
+require 'impostor/answer'
+require 'impostor/player'
+
+DataMapper.finalize
+DataMapper.auto_upgrade!
