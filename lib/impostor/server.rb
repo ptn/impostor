f309308a6f7c743696aa480d86673014e7561d8d
Pablo A. Torres Navarrete <tn.pablo@gmail.com>
2012-01-21 22:32:41 -0500

    Server.process returns the number of processed emails

--- lib/impostor/server.rb
+++ lib/impostor/server.rb
@@ -5,18 +5,21 @@ require_relative 'commands'
 module Impostor
   module Server
     #
-    # For at least the last <count> unread emails in the server, fire off the
+    # For at least the last +count+ unread emails in the server, fire off the
     # corresponding command.
     #
     # +count+ can be an integer or :all
     #
-    #TODO Should return something useful
+    # Returns the number of emails processed.
+    #
     def self.process(count, parser=Parsers::EmailParser.new)
       unread = mailer.get_unread(count)
       unread.each do |ur|
         args = parser.parse(ur.from.first, ur.subject, ur.body.decoded)
         Command.new.run *args
       end
+
+      unread.count
     end
 
     private
